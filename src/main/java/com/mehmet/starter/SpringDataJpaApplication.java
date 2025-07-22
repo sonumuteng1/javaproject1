06409b6fc22d6d5f3cbae47af293db6abf50036f
Mehmet Emin <karagozfamily1@gmail.com>
2025-07-22 10:38:40 +0200

    second commit for test

--- src/main/java/com/mehmet/starter/SpringDataJpaApplication.java
+++ src/main/java/com/mehmet/starter/SpringDataJpaApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
-//Bu paketin adını starter olarak değiştirdim
+//Bu paketin adını starter olarak değiştirdim.
+	//test second commit
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataJpaApplication.class, args);
 	}
