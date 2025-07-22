e22cbf3333f48be1a64fc77e524b3274986518b8
Mehmet Emin <karagozfamily1@gmail.com>
2025-07-22 11:33:37 +0200

    h2 database and student entity have been created

---
+++ src/main/java/com/mehmet/entites/Student.java
@@ -0,0 +1,34 @@
+package com.mehmet.entites;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="student")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name = "birt_of_date")
+	private Date birthOfDate;
+	
+	
+}
--- src/main/java/com/mehmet/starter/SpringDataJpaApplication.java
+++ src/main/java/com/mehmet/starter/SpringDataJpaApplication.java
@@ -2,7 +2,10 @@ package com.mehmet.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+//Entity scan anotasyonu tablo oluşturulması için önemli.
+@EntityScan(basePackages = "com.mehmet")
 @SpringBootApplication
 public class SpringDataJpaApplication {
 //Bu paketin adını starter olarak değiştirdim.
