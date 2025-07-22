c3c339d586b9946b2d09d54517442da5d4c51245
Mehmet Emin <karagozfamily1@gmail.com>
2025-07-22 10:28:01 +0200

    first commit

---
+++ src/main/java/com/mehmet/starter/SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mehmet.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+//Bu paketin adını starter olarak değiştirdim
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaApplication.class, args);
+	}
+
+}
