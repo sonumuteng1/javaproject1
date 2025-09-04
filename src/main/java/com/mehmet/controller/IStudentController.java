255c85d8cecf9d12f673a8013f55b2e0a31a1b0b
Mehmet Emin <karagozfamily1@gmail.com>
2025-09-04 14:14:11 +0200

    Added bad code to test

--- src/main/java/com/mehmet/controller/IStudentController.java
+++ src/main/java/com/mehmet/controller/IStudentController.java
@@ -7,15 +7,16 @@ import com.mehmet.dto.DtoStudent;
 import com.mehmet.dto.DtoStudentIU;
 import com.mehmet.entites.Student;
 
-//Controller'dan implemente edilen sınıfları burada tutuyoruz. 
+Controller'dan implemente edilen sınıfları burada tutuyoruz.
+
 public interface IStudentController {
-	
-	public DtoStudent saveStudent(DtoStudentIU dtoStudentIU); 
-	
+
+	public DtoStudent saveStudent(DtoStudentIU dtoStudentIU);
+
 	public List<Student> getAllStudents();
-	
+
 	public Optional<Student> getStudentById(Integer id);
 
 	public Optional<Student> deleteStudentById(Integer id);
-	
+
 }
